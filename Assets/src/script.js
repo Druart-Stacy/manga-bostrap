4ed06a5899c583b9e9d1d508c7931305359872f4
Stacy Druart <stacy@StacyDruartMSI>
2024-11-21 14:27:10 +0100

    maj

--- Assets/src/script.js
+++ Assets/src/script.js
@@ -2,26 +2,26 @@
  * Fonction pour rechercher dans les titres et descriptions des livres
  */
 
-const sampleBooks = [
-    {
-        id: 1,
-        title: "Naruto",
-        description: "Un ninja avec un rêve de devenir Hokage.",
-        image: "https://via.placeholder.com/150"
-    },
-    {
-        id: 2,
-        title: "One Piece",
-        description: "Une aventure pour trouver le One Piece.",
-        image: "https://via.placeholder.com/150"
-    },
-    {
-        id: 3,
-        title: "Demon Slayer",
-        description: "Un garçon combat des démons pour sauver sa sœur.",
-        image: "https://via.placeholder.com/150"
-    }
-];
+// const sampleBooks = [
+//     {
+//         id: 1,
+//         title: "Naruto",
+//         description: "Un ninja avec un rêve de devenir Hokage.",
+//         image: "https://via.placeholder.com/150"
+//     },
+//     {
+//         id: 2,
+//         title: "One Piece",
+//         description: "Une aventure pour trouver le One Piece.",
+//         image: "https://via.placeholder.com/150"
+//     },
+//     {
+//         id: 3,
+//         title: "Demon Slayer",
+//         description: "Un garçon combat des démons pour sauver sa sœur.",
+//         image: "https://via.placeholder.com/150"
+//     }
+// ];
 
 // Enregistrer dans localStorage
 localStorage.setItem('books', JSON.stringify(sampleBooks));
